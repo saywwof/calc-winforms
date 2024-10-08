1b70a1121a889bf9a35c8e1d94c40e800e7ad39d
Student406-11 <Student406-11@NPK-102O-4>
2024-10-08 13:47:16 +0700

    Добавьте файлы проекта.

---
+++ calc winforms/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc_winforms
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Mul(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Div(double a, double b)
+        {
+            if (b != 0)
+            {
+                return a / b;
+            }
+            else
+            {
+                throw new DivideByZeroException("Ошибка: деление на ноль!");
+            }
+        }
+    }
+}
---
+++ calc winforms/CalculatorForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calc_winforms
+{
+    public partial class CalculatorForm : Form
+    {
+        private Calculator calc = new Calculator();
+
+        public CalculatorForm()
+        {
+            InitializeComponent();
+            calc = new Calculator();
+        }
+
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            textBox3.Text = calc.Add(a, b).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            textBox3.Text = calc.Sub(a, b).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            textBox3.Text = calc.Mul(a, b).ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            if (b != 0)
+            {
+                textBox3.Text = calc.Div(a, b).ToString();
+            }
+            else
+            {
+                textBox3.Text = "Ошибка: деление на ноль!";
+            }
+        }
+    }
+}
